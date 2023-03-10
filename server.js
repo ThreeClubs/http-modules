19a5c699108d1f14b957d4d9da54ef64f91676b2
Andrew Grenon <andrew.grenon@gmail.com>
2023-03-10 14:19:18 +1300

    init commit http mod GET only

---
+++ server.js
@@ -0,0 +1,17 @@
+// 1) declare a variable called http and import "http" inside require
+
+// 2)create a variable called "server" and use http and createServer method then pass in "req" and "res" as params
+//using res.write and res.end, write some code that displays "Hello" when we visit our server on the browser
+
+// 3) use our server with .listen to check that we're running and console log a server listen message such as "Server has started on 4000" etc
+
+// 4) Write a conditional statement that checks if we are at "/" (localhost:4000/ etc..)
+// and if we are NOT there ie we write something like "/blah" then we get a 404 error
+
+// 5) add an additional condition which checks for an another endpoint of your own creation such as "/stuff" or "/message"
+// you can make your own api and populate it with that, or you can just put in whatever you like!
+// share your unique content with the class!
+
+// 6) BONUS rewrite the code using writeHead and defind content type, then put html elements directly in the end statement to give your content
+// some style and structure
+
